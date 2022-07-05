0a59e2666b25a55ab14f744c767cd5fc5ed496a3
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-07-05 16:48:21 +0200

    scaffolding frontoffice

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -27,3 +24,7 @@ Route::middleware('auth')
     ->group(function() {
        Route::get('/', 'HomeController@index')->name('home'); 
     });
+
+Route::get( '{any?}', function() {
+    return view('guest.home');
+})->where('any', '.*');
